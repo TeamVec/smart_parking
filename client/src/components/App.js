6be27c70bd0dce9eba7184747044b404ee5179ea
Piyush Soni <piyushsoni0069@gmail.com>
2020-12-26 19:16:21 +0530

    Css added
    Home styling change

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import {Router,Route,Switch } from 'react-router-dom'
 import Header from './Header'
 import Home from './Home'
+import './App.css'
 import history from '../history'
 
 function App() {
--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './Header.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const Header=()=> {
     return (
-        <div style={{backgroundColor: "grey", padding:"10px 32px"}}>
+        <div className="navbar" style={{backgroundColor: "grey", padding:"10px 32px"}}>
             <Link style={{textDecoration:'none'}} to='/' className='item'>
                 <h1><img src="https://cdn.iconscout.com/icon/free/png-512/p-6-675564.png" alt='' height="50px" className="grayscale"/>
                <span style={{color:'white'}}>ark</span>Here</h1>
